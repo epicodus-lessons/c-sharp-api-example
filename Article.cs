973f142b6afb07b3deb1bfdd440464f961a8d4a4
Elly Maimon <eliranmaimon@gmail.com>
2019-09-25 10:07:36 -0700

    Deserializing Responses

---
+++ Article.cs
@@ -0,0 +1,11 @@
+namespace ApiTest
+{
+    class Article
+    {
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Url { get; set; }
+        public string Byline { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ApiTest
 {
@@ -10,7 +13,18 @@ namespace ApiTest
     {
       var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
       var result = apiCallTask.Result;
-      Console.WriteLine(result);
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+      }
     }
   }
 
