49d580f6c389a5a1c93fcc61827eefeb771c0439
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-30 20:49:24 -0700

    add commentary and two more examples of deserializing

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace ApiTest
       var result = apiCallTask.Result;
 
       JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+
+      // Grabbing and displayinng all ojects in the results array
       List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
 
       foreach (Article article in articleList)
@@ -24,6 +26,21 @@ namespace ApiTest
         Console.WriteLine($"Abstract: {article.Abstract}");
         Console.WriteLine($"Url: {article.Url}");
         Console.WriteLine($"Byline: {article.Byline}");
+      }
+
+      // Grabbing and displaying num_results property in jsonResponse object and storing it in the MetaData class.
+      MetaData metaData = JsonConvert.DeserializeObject<MetaData>(jsonResponse.ToString()); 
+      Console.WriteLine($"Num_results: {metaData.Num_Results}");
+
+      // Grabbing and displaying all objects in the multimedia array within the first object in the results array, and storing them in a List of the Multimedia type.
+      List<Multimedia> multimediaList = JsonConvert.DeserializeObject<List<Multimedia>>(jsonResponse["results"][0]["multimedia"].ToString());
+      foreach (Multimedia media in multimediaList)
+      {
+        Console.WriteLine("---------");
+        Console.WriteLine($"MULTIMEDIA");
+        Console.WriteLine($"Type: {media.Type}");
+        Console.WriteLine($"SubType: {media.SubType}");
+        Console.WriteLine($"Caption: {media.Caption}");
       }
     }
   }
