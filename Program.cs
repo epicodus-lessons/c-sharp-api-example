4dc3f644778d940e8e8e2e0a12a874ad82e32dad
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-30 21:05:46 -0700

    refactor code to grab and display results objects and their nested multimedia objects from jsonResponse

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ApiTest
 
       JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
 
-      // Grabbing and displayinng all ojects in the results array
+      // Grabbing and displaying all ojects in the results array INCLUDING all objects within the nested multimedia array
       List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
 
       foreach (Article article in articleList)
@@ -26,22 +26,20 @@ namespace ApiTest
         Console.WriteLine($"Abstract: {article.Abstract}");
         Console.WriteLine($"Url: {article.Url}");
         Console.WriteLine($"Byline: {article.Byline}");
+        foreach (Multimedia media in article.Multimedia)
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine($"MULTIMEDIA");
+            Console.WriteLine($"Type: {media.Type}");
+            Console.WriteLine($"SubType: {media.SubType}");
+            Console.WriteLine($"Caption: {media.Caption}");
+        }
+        Console.WriteLine("__________________________________");
       }
 
       // Grabbing and displaying num_results property in jsonResponse object and storing it in the MetaData class.
       MetaData metaData = JsonConvert.DeserializeObject<MetaData>(jsonResponse.ToString()); 
       Console.WriteLine($"Num_results: {metaData.Num_Results}");
-
-      // Grabbing and displaying all objects in the multimedia array within the first object in the results array, and storing them in a List of the Multimedia type.
-      List<Multimedia> multimediaList = JsonConvert.DeserializeObject<List<Multimedia>>(jsonResponse["results"][0]["multimedia"].ToString());
-      foreach (Multimedia media in multimediaList)
-      {
-        Console.WriteLine("---------");
-        Console.WriteLine($"MULTIMEDIA");
-        Console.WriteLine($"Type: {media.Type}");
-        Console.WriteLine($"SubType: {media.SubType}");
-        Console.WriteLine($"Caption: {media.Caption}");
-      }
     }
   }
 
