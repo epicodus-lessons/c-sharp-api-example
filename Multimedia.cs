fbc607009dcf7c0369a22123120eeb5b4f7ff9c3
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-30 20:53:32 -0700

    add Multimedia model for the objects in the  multimedia array of the jsonResponse object

---
+++ Multimedia.cs
@@ -0,0 +1,9 @@
+namespace ApiTest
+{
+    public class Multimedia
+    { 
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public string Caption { get; set; }
+    }
+}
